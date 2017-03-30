6911f8371225afb3af42a520d13fcfc88626b852
chenyong <784592715@qq.com>
2017-03-30 11:36:35 +0800

    添加登录、创建帐号、密码重置界面

--- RobotSmailHelper/app/src/main/java/com/jeff/robotsmailhelper/login/LoginActivity.java
+++ RobotSmailHelper/app/src/main/java/com/jeff/robotsmailhelper/login/LoginActivity.java
@@ -1,21 +1,132 @@
 package com.jeff.robotsmailhelper.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jeff.robotsmailhelper.R;
+import com.jeff.robotsmailhelper.chat.view.activity.ChatActivity;
+import com.jeff.robotsmailhelper.utils.RegexUtils;
+import com.jeff.robotsmailhelper.views.CleanEditText;
 
 /**
  * 登录
  * Created by chen_yong on 2017/3/30.
  */
 
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CleanEditText accountEdit;
+    private CleanEditText passwordEdit;
+    private long mExitTime = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        initViews();
+    }
+
+    private void initViews() {
+        accountEdit = (CleanEditText) this.findViewById(R.id.et_email_phone);
+        accountEdit.setImeOptions(EditorInfo.IME_ACTION_NEXT);
+        accountEdit.setTransformationMethod(HideReturnsTransformationMethod
+                .getInstance());
+        passwordEdit = (CleanEditText) this.findViewById(R.id.et_password);
+        passwordEdit.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        passwordEdit.setImeOptions(EditorInfo.IME_ACTION_GO);
+        passwordEdit.setTransformationMethod(PasswordTransformationMethod
+                .getInstance());
+        passwordEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId,
+                                          KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE
+                        || actionId == EditorInfo.IME_ACTION_GO) {
+                    clickLogin();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void clickLogin() {
+        String account = accountEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
+        if (checkInput(account, password)) {
+            // TODO: 请求服务器登录账号
+        }
+    }
+
+    /**
+     * 检查输入
+     *
+     * @param account  用户名
+     * @param password 密码
+     * @return
+     */
+    public boolean checkInput(String account, String password) {
+        // 账号为空时提示
+        if (account == null || account.trim().equals("")) {
+            Toast.makeText(this, R.string.tip_account_empty, Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            // 账号不匹配手机号格式（11位数字且以1开头）
+            if (!RegexUtils.checkMobile(account)) {
+                Toast.makeText(this, R.string.tip_account_regex_not_right,
+                        Toast.LENGTH_LONG).show();
+            } else if (password == null || password.trim().equals("")) {
+                Toast.makeText(this, R.string.tip_password_can_not_be_empty,
+                        Toast.LENGTH_LONG).show();
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login:
+//                clickLogin();
+                startActivity(new Intent(this, ChatActivity.class));
+                finish();
+                break;
+            case R.id.tv_create_account:
+                startActivity(SignUpActivity.createSignUpExplicitIntent(this));
+                break;
+            case R.id.tv_forget_password:
+                startActivity(ForgetPasswordActivity.createForgetPasswordExplicitIntent(this));
+                break;
+            default:
+                break;
+        }
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if ((System.currentTimeMillis() - mExitTime) > 1000) {
+                // 如果两次按键时间间隔大于2000毫秒，则不退出
+                Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                mExitTime = System.currentTimeMillis();// 更新mExitTime
+            } else {
+                System.exit(0);// 否则退出程序
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+
     }
 }
