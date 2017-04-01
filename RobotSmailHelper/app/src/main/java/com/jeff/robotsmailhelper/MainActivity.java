d178745ad19529a8dba325399e50fad2a817ea6a
chenyong <784592715@qq.com>
2017-04-01 17:22:02 +0800

    添加一个页面

---
+++ RobotSmailHelper/app/src/main/java/com/jeff/robotsmailhelper/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jeff.robotsmailhelper;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * main_activity
+ * Created by chen_yong on 2017/4/1.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
